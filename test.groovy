d418b98f11992993f62e1659a4b00230d0cf6503
Samuel <zerzamo@gmail.com>
2025-10-17 11:50:03 +0200

    modification

--- test.groovy
+++
@@ -1,9 +0,0 @@
-import java.nio.file.Files
-import java.nio.file.Paths
-
-def filePath = Paths.get("server.csv")
-def lines = Files.readAllLines(filePath)
-
-lines.each { line ->
-    println line
-}
